eee10539412d468b1cce275f058652257f387749
Chris Allen <chrisallen2001@gmail.com>
2017-02-19 20:30:43 -0600

    Began db pull to recycler view

--- app/src/main/java/com/apexcomputerservice/legotracker/DatabaseHelper.java
+++ app/src/main/java/com/apexcomputerservice/legotracker/DatabaseHelper.java
@@ -3,6 +3,7 @@ package com.apexcomputerservice.legotracker;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -512,6 +513,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         if (db != null && db.isOpen())
             db.close();
+    }
+
+    //************ Open Database *********************************8
+
+    public DatabaseHelper openReadableDB() throws SQLException{
+        SQLiteDatabase db = this.getReadableDatabase();
+        return this;
+    }
+
+    public DatabaseHelper openWriteableDB() throws SQLException{
+        SQLiteDatabase db = this.getWritableDatabase();
+        return this;
     }
 
 }
---
+++ app/src/main/java/com/apexcomputerservice/legotracker/RVAdapterChain.java
@@ -0,0 +1,69 @@
+package com.apexcomputerservice.legotracker;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.apexcomputerservice.legotracker.model.Chain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris on 2/18/2017.
+ */
+
+public class RVAdapterChain extends RecyclerView.Adapter<RVAdapterChain.ChainViewHolder> {
+
+    public static class ChainViewHolder extends RecyclerView.ViewHolder{
+
+        CardView cv;
+        TextView chainName;
+
+
+        ChainViewHolder(View itemView){
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv_chain);
+            chainName = (TextView)itemView.findViewById(R.id.chain_name);
+        }
+    }
+
+    DatabaseHelper myDatabaseHelper = new DatabaseHelper(RVAdapterChain.this);
+    myDatabaseHelper.openReadableDB();
+    List<Chain> chains = myDatabaseHelper.getChains();
+    myDatabaseHelper.closeDB();
+
+    //ArrayList<Chain> chains;
+
+    RVAdapterChain(ArrayList<Chain> chains){
+        this.chains=chains;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView){
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public ChainViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+
+
+
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_chain,viewGroup,false);
+        ChainViewHolder cvh = new ChainViewHolder(v);
+        return cvh;
+    }
+
+    @Override
+    public void onBindViewHolder(ChainViewHolder chainViewHolder, int i){
+        chainViewHolder.chainName.setText(chains.get(i).getChainName());
+    }
+
+    @Override
+    public int getItemCount(){
+        return chains.size();
+    }
+}
